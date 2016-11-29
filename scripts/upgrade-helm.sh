a417b12244e7e06c2a645d3d5362c87a3c05ab59
Gavin Zhou <gavin.zhou@gmail.com>
2016-11-29 22:41:58 +0900

    fix gcr url

---
+++ scripts/upgrade-helm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+kubectl delete deployment --namespace kube-system tiller-deploy
+curl -sSL http://storage.googleapis.com/kubernetes-helm/helm-v2.0.0-rc.2-darwin-amd64.tar.gz |tar -xz 
+mv darwin-amd64/helm /usr/local/sbin/
+helm init
+sleep 5
+helm version
