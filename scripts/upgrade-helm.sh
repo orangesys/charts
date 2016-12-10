3ec94f4e5ad83310b9eb38228734930e94ea72c5
Gavin Zhou <gavin.zhou@gmail.com>
2016-12-10 23:08:13 +0900

    verup helm

--- scripts/upgrade-helm.sh
+++ scripts/upgrade-helm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 kubectl delete deployment --namespace kube-system tiller-deploy
-curl -sSL http://storage.googleapis.com/kubernetes-helm/helm-v2.0.0-rc.2-darwin-amd64.tar.gz |tar -xz 
+curl -sSL http://storage.googleapis.com/kubernetes-helm/helm-v2.0.2-darwin-amd64.tar.gz|tar -xz
 mv darwin-amd64/helm /usr/local/sbin/
 helm init
 sleep 5
