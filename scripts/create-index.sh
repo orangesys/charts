6a02f01fea4578535e8e91f6dc1d2c7deb532185
Gavin Zhou <gavin.zhou@gmail.com>
2017-08-06 23:50:03 +0900

    create helm repo with github

--- scripts/create-index.sh
+++ scripts/create-index.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+set -ex
 helm repo index orangesys-io-charts --url https://storage.googleapis.com/orangesys-io-charts
+helm repo index docs --url https://orangesys.github.io/charts
--- scripts/helm-package.sh
+++ scripts/helm-package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 _packagelist=(adduser influxdb traefik grafana kong postgres slack8s orangesys-srv orangeapi)
 if [ $# -gt 0 ]; then
@@ -9,6 +9,8 @@ fi
 
 for _d in "${_packagelist[@]}"
 do
- rm -rf  orangesys-io-charts/${_d}*.tgz
+ rm -rf orangesys-io-charts/${_d}*.tgz
+ rm -rf docs/${_d}.tgz
  helm package ${_d} && mv ${_d}*.tgz orangesys-io-charts/
+ helm package ${_d} && mv ${_d}*.tgz docs/
 done
