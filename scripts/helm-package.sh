c096de366571f3be90903d0f153149f011efd0ce
gavinzhou <gavin.zhou@gmail.com>
2017-08-07 17:29:02 +0900

    fix mistake

--- scripts/helm-package.sh
+++ scripts/helm-package.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+rm -rf docs/${_d}.tgz
 
 _packagelist=(influxdb traefik grafana kong postgres slack8s orangeapi kube-cert-manager kubebot)
 if [ $# -gt 0 ]; then
@@ -10,7 +11,6 @@ fi
 for _d in "${_packagelist[@]}"
 do
  #rm -rf orangesys-io-charts/${_d}*.tgz
- rm -rf docs/${_d}.tgz
  #helm package ${_d} && mv ${_d}*.tgz orangesys-io-charts/
  helm package ${_d} && mv ${_d}*.tgz docs/
 done
