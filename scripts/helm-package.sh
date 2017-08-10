c4a37aa22a6112c3b95dd6f9155bfef564e229c3
Gavin Zhou <gavin.zhou@gmail.com>
2017-08-10 23:09:27 +0900

    verup

--- scripts/helm-package.sh
+++ scripts/helm-package.sh
@@ -3,14 +3,12 @@
 set -e
 rm -rf docs/${_d}.tgz
 
-_packagelist=(influxdb traefik grafana kong postgres slack8s orangeapi kube-cert-manager kubebot)
+_packagelist=(influxdb grafana)
 if [ $# -gt 0 ]; then
   _packagelist=("$@")
 fi
 
 for _d in "${_packagelist[@]}"
 do
- #rm -rf orangesys-io-charts/${_d}*.tgz
- #helm package ${_d} && mv ${_d}*.tgz orangesys-io-charts/
  helm package ${_d} && mv ${_d}*.tgz docs/
 done
